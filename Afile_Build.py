5d8b7187640128b7b27c97fc96882c4561b707f6
stealthydonut <kinger6688@gmail.com>
2017-05-24 15:47:20 -0400

    Added ticker
    This file estimates the future value of percentages

--- Afile_Build.py
+++ Afile_Build.py
@@ -7,6 +7,24 @@ if sys.version_info[0] < 3:
 else:
     from io import StringIO as stio
 
+###########################
+#Get the ticker file with %
+###########################
+from google.cloud import storage
+client = storage.Client()
+bucket = client.get_bucket('gdxjtrade')
+# Then do other things...
+blob = bucket.get_blob('GDXJ_TICKERS.csv')
+content = blob.download_as_string()
+#Because the pandas dataframe can only read from buffers or files, we need to take the string and put it into a buffer
+inMemoryFile = StringIO.StringIO()
+inMemoryFile.write(content)
+#When you buffer, the "cursor" is at the end, and when you read it, the starting position is at the end and it will not pick up anything
+inMemoryFile.seek(0)
+#Note - anytime you read from a buffer you need to seek so it starts at the beginning
+#The low memory false exists because there was a lot of data
+ticker=pd.read_csv(inMemoryFile, low_memory=False)    
+    
 #######################
 #Get the daily holdings
 #######################
@@ -46,7 +64,8 @@ pricechanges=pd.read_csv(inMemoryFile, low_memory=False)
 ###############################
 #Merge files by date and ticker
 ###############################
-outputfile=pd.merge(pricechanges, holdings, how='right', left_on=['VG Ticker','date'], right_on=['Ticker','Date'])
+out1=pd.merge(pricechanges, ticker, how='left', left_on=['VG Ticker'], right_on=['VG Ticker'])
+outputfile=pd.merge(out1, holdings, how='right', left_on=['VG Ticker','date'], right_on=['Ticker','Date'])
 
 ######################
 #Begin to do Analytics
