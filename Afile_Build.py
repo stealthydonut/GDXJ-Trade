0854e2ca926d06baec157265147bb8b3c19d5969
stealthydonut <kinger6688@gmail.com>
2017-05-25 09:14:02 -0400

    Added multiple price files

--- Afile_Build.py
+++ Afile_Build.py
@@ -59,7 +59,19 @@ inMemoryFile.write(content)
 inMemoryFile.seek(0)
 #Note - anytime you read from a buffer you need to seek so it starts at the beginning
 #The low memory false exists because there was a lot of data
-pricechanges=pd.read_csv(inMemoryFile, low_memory=False)
+pricechanges1=pd.read_csv(inMemoryFile, low_memory=False)
+# Then do other things...
+blob = bucket.get_blob('daily_prices2017_5_24.csv')
+content = blob.download_as_string()
+#Because the pandas dataframe can only read from buffers or files, we need to take the string and put it into a buffer
+inMemoryFile = StringIO.StringIO()
+inMemoryFile.write(content)
+#When you buffer, the "cursor" is at the end, and when you read it, the starting position is at the end and it will not pick up anything
+inMemoryFile.seek(0)
+#Note - anytime you read from a buffer you need to seek so it starts at the beginning
+#The low memory false exists because there was a lot of data
+pricechanges2=pd.read_csv(inMemoryFile, low_memory=False)
+pricechanges = pricechanges1.append(pricechanges2, ignore_index=True)
 
 ###############################
 #Merge files by date and ticker
