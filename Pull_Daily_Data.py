c0813e273bdbd6f7db1360b73253023cb8772cd1
stealthydonut <kinger6688@gmail.com>
2017-05-25 15:44:20 -0400

    Update Pull_Daily_Data.py

--- Pull_Daily_Data.py
+++ Pull_Daily_Data.py
@@ -65,7 +65,7 @@ fix=name+stamp+end
 
 #Add the join key so it can be joined back to the holdings file
 holdings_ticker=df[['VG Ticker','Ticker']]
-holdings_ticker.loc[[1 for range(0, len(holdings_ticker['Ticker']))],'Ticker'].map(lambda x: x.strip())
+holdings_ticker.loc[[1 for x in range(0, len(holdings_ticker['Ticker']))],'Ticker'].map(lambda x: x.strip())
 daily_prices.loc['Ticker'].map(lambda x: x.strip())
 outputfile=pd.merge(daily_prices, holdings_ticker, how='left', left_on=['Ticker'], right_on=['Ticker'])
 
