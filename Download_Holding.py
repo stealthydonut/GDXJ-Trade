edb0bb1e5e05422ed7433764b223bd3dfdbdc5c6
stealthydonut <kinger6688@gmail.com>
2017-05-26 10:22:57 -0400

    Read in html file

--- Download_Holding.py
+++ Download_Holding.py
@@ -1,5 +1,9 @@
+import datetime
+import xlrd
 import urllib
+import lxml
 dls = "https://www.vaneck.com/FundHoldings.aspx?ticker=GDXJ"
 urllib.urlretrieve(dls, "test.xls")
-
-print dls
+data = pd.read_html('test.xls', skiprows=2)
+df = data[0]
+df.columns = ['number','holding','Ticker','Shares','Market Value','Per net assets']
