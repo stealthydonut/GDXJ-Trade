c7a14a4b83977d37cae04a3a440503b335f23fc6
stealthydonut <kinger6688@gmail.com>
2017-05-28 10:46:46 -0400

    Update HTML_PARSE.py

--- HTML_PARSE.py
+++ HTML_PARSE.py
@@ -1,1 +1,10 @@
+#This program parces out information from the shortdata website
+from lxml import html
+import requests
 
+page = requests.get('http://shortdata.ca/?c=guyana-goldfields%20inc.-short-sales&sym=GUY.TO')
+tree = html.fromstring(page.content)
+
+#This will create a list of buyers:
+values = tree.xpath('<td>"data"</td>')
+<td>"data"</td>
