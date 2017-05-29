a8bc18ab4990cd4e92b6c172b5f274aae093d9a2
stealthydonut <kinger6688@gmail.com>
2017-05-29 13:34:59 -0400

    Update Afile_Build.py

--- Afile_Build.py
+++ Afile_Build.py
@@ -43,9 +43,10 @@ inMemoryFile.seek(0)
 #Note - anytime you read from a buffer you need to seek so it starts at the beginning
 #The low memory false exists because there was a lot of data
 holdings=pd.read_csv(inMemoryFile, low_memory=False)
+#holdings.drop_duplicates(cols='Date', take_last=True)
 
 ############################
-#Get the daily price changes
+#Get the short and float positions
 ############################
 from google.cloud import storage
 client = storage.Client()
@@ -60,15 +61,32 @@ inMemoryFile.write(content)
 inMemoryFile.seek(0)
 #Note - anytime you read from a buffer you need to seek so it starts at the beginning
 #The low memory false exists because there was a lot of data
-df=pd.read_csv(inMemoryFile, low_memory=False)
-floatchanges=df[['Ticker','VG Ticker','Float Shares','Short Ratio','date']]
+floatchanges=pd.read_csv(inMemoryFile, low_memory=False)
 
+############################
+#Get the historical prices
+############################
+from google.cloud import storage
+client = storage.Client()
+bucket = client.get_bucket('gdxjtrade')
+# Then do other things...
+blob = bucket.get_blob('gdxj_historical.csv')
+content = blob.download_as_string()
+#Because the pandas dataframe can only read from buffers or files, we need to take the string and put it into a buffer
+inMemoryFile = StringIO.StringIO()
+inMemoryFile.write(content)
+#When you buffer, the "cursor" is at the end, and when you read it, the starting position is at the end and it will not pick up anything
+inMemoryFile.seek(0)
+#Note - anytime you read from a buffer you need to seek so it starts at the beginning
+#The low memory false exists because there was a lot of data
+pricechanges=pd.read_csv(inMemoryFile, low_memory=False)
 
 ###############################
 #Merge files by date and ticker
 ###############################
-out1=pd.merge(floatchanges, ticker, how='left', left_on=['VG Ticker'], right_on=['VG Ticker'])
-outputfile=pd.merge(out1, holdings, how='right', left_on=['VG Ticker','date'], right_on=['Ticker','Date'])
+out1=pd.merge(floatchanges, ticker, how='left', left_on=['VG Ticker_y'], right_on=['VG Ticker'])
+out2=pd.merge(out1, holdings, how='right', left_on=['VG Ticker','date'], right_on=['Ticker','Date'])
+outputfile=pd.merge(pricechanges, out2, how='right', left_on=['VG Ticker','Date'], right_on=['VG Ticker','date'])
 
 ######################
 #Begin to do Analytics
